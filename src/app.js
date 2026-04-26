a5847bdece2709bad83803f20e4a487f03b8b80a
vansh-2110-garg <garg05vansh@gmail.com>
2026-04-26 15:01:36 +0530

    created an express server

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use("/test",(req,res)=>{
+    res.send("Hello !");
+});
+
+app.listen(3000,()=>{
+    console.log("Server is successfully listening on port 3000...");
+});
