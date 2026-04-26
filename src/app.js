da518c19756e5f2189adab28e2c44630b06b6fe9
vansh-2110-garg <garg05vansh@gmail.com>
2026-04-26 16:32:06 +0530

    Exploring HTTP methods

--- src/app.js
+++ src/app.js
@@ -2,8 +2,18 @@ const express = require('express');
 
 const app = express();
 
+app.get("/user",(req,res)=>{
+    res.send({firstName:"Vansh",lastName:"Garg"});
+});
+app.post("/user",(req,res)=>{
+    console.log("save the data to the database");
+    res.send("Data successfully saved to database");
+});
+app.delete("/user",(req,res)=>{
+    res.send("Data deleted successfully");
+});
 app.use("/test",(req,res)=>{
-    res.send("Hello !");
+    res.send("Hello From the server!");
 });
 
 app.listen(3000,()=>{
