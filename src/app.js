c4f982ad960c3a1c836df25a14182477a92775e8
vansh-2110-garg <garg05vansh@gmail.com>
2026-04-29 00:40:03 +0530

    Playing With Route Handlers

--- src/app.js
+++ src/app.js
@@ -3,10 +3,33 @@ const express = require('express');
 const app = express();
 
 
-app.get("/user/:userID/:name/:password",(req,res)=>{
-    console.log(req.params);
-    res.send({firstName:"Vansh",lastName:"Garg"});
-});
+app.get("/user",
+    (req,res,next)=>{
+        console.log("1");
+        res.send("1");
+        next();
+    },
+    (req,res,next)=>{
+        console.log("2");
+        // res.send("2");
+        next();
+    },
+    (req,res,next)=>{
+        console.log("3");
+        // res.send("3");
+        next();
+    },
+    (req,res,next)=>{
+        console.log("4");
+        // res.send("4");
+        next();
+    },
+    (req,res,next)=>{
+        console.log("5");
+        // res.send("5");
+        next();
+    }
+)
 
 
 app.listen(3000,()=>{
