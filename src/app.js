dd7748b4d358911ba9789ad2048c0e3c0572cca5
vansh-2110-garg <garg05vansh@gmail.com>
2026-05-01 01:31:34 +0530

    get apis

--- src/app.js
+++ src/app.js
@@ -20,6 +20,32 @@ app.post("/signup",async(req,res)=>{
     }
 });
 
+app.get("/user",async (req,res)=>{
+    const userEmail=req.body.email;
+    try{
+        const users=await User.find({email:userEmail});
+        if(users.length==0){
+            res.status(404).send("User not found");
+        }
+        res.send(users);
+    }
+    catch(err){
+        res.status(400).send("something went wrong");
+    }
+})
+
+// Feed API - GET /feed - get all the users from the database
+app.get("/feed",async(req,res)=>{
+    
+    try{
+        const users=await User.find({});
+        res.send(users);
+    }
+    catch(err){
+        res.status(400).send("something went wrong");
+    }
+})
+
 connectDb().then(()=>{
     console.log("database connected successfully");
     app.listen(process.env.PORT,()=>{
